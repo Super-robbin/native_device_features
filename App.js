cc3de94891d19fcf983c4cb7e96eea690632025d
Roberto <roberto.quadraccia90@gmail.com>
2024-08-26 14:35:41 +0200

    SQLite installed and configured

--- App.js
+++ App.js
@@ -8,13 +8,38 @@ import Map from "./screens/Map";
 
 import IconButton from "./components/UI/IconButton";
 import { Colors } from "./constants/colors";
+import { useEffect, useState } from "react";
+import { init } from "./util/database";
+import * as SplashScreen from "expo-splash-screen";
 
 const Stack = createNativeStackNavigator();
+
+SplashScreen.preventAutoHideAsync();
 
 // IMPORTANT: To be able to use navigation inside the AllPlaces screen from here (App),
 // we need to create options below as a function that receives an object.
 // This way we are able to pass navigation inside options and have it available for the onPress
+
+// init() - We initialise the database as soon as the App componenent is rendered
 export default function App() {
+  const [dbInitialised, setDbInitialised] = useState(dalse);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialised(true);
+        // Hide the splash screen after the database is initialized
+        SplashScreen.hideAsync();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!dbInitialised) {
+    return null; // Keep the splash screen visible while loading
+  }
+
   return (
     <>
       <StatusBar style="dark" />
